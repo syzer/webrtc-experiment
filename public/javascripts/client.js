26c221eacd4fc0de8028644607ed65510c4eefa0
Joel Richard <joelrich@adobe.com>
2014-04-24 19:45:48 +0200

    Listen to websocket connection and update user list

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,4 +1,6 @@
 (function () {
+    var users = {};
+
     var socket = new WebSocket("ws://" + location.host + "/connect");
     socket.onopen = function () {
 
@@ -7,12 +9,19 @@
         var message = JSON.parse(e.data);
         switch (message.type) {
             case "user-connected":
-
+                var username = message.username;
+                var $user = $("li").text(username);
+                users[message.user] = {name: username, $el: $user};
+                $("ul.users").append($user);
                 break;
             case "user-disconnected":
-
+                var user = users[message.user];
+                if (user) {
+                    user.$el.remove();
+                }
                 break;
         }
     };
+
 })();
 
