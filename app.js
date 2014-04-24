1bd7c282b6af13150965fc533604a602ed84d169
EGintowt <egintowt@gmail.com>
2014-04-24 19:47:47 +0200

    Added: WebRTC.io

--- app.js
+++ app.js
@@ -36,3 +36,67 @@ http.createServer(app).listen(app.get('port'), function(){
 var webRTC = require('webrtc.io').listen(8001);
 //then a bunch of callbacks are available
 console.log('WebRTC server listening on port 8001');
+
+var app = require('express')();
+var server = require('http').createServer(app);
+var webRTC = require('webrtc.io').listen(server);
+
+var port = process.env.PORT || 8080;
+server.listen(port);
+
+var app = require('express')();
+var server = require('http').createServer(app);
+var webRTC = require('webrtc.io').listen(server);
+
+var port = process.env.PORT || 8080;
+server.listen(port);
+
+
+webRTC.rtc.on('chat_msg', function(data, socket) {
+    var roomList = webRTC.rtc.rooms[data.room] || [];
+
+    for (var i = 0; i < roomList.length; i++) {
+        var socketId = roomList[i];
+
+        if (socketId !== socket.id) {
+            var soc = webRTC.rtc.getSocket(socketId);
+
+            if (soc) {
+                soc.send(JSON.stringify({
+                    "type": 3,
+                    "eventName": "receive_chat_msg",
+                    "data": {
+                        "messages": data.messages,
+                        "color": data.color
+                    }
+                }), function(error) {
+                    if (error) {
+                        console.log(error);
+                    }
+                });
+            }
+        }
+    }
+});
+
+//app.get('/', function(req, res) {
+//    res.sendfile(__dirname + '/index.html');
+//});
+//
+//app.get('/style.css', function(req, res) {
+//    res.sendfile(__dirname + '/style.css');
+//});
+//
+//app.get('/fullscrean.png', function(req, res) {
+//    res.sendfile(__dirname + '/fullscrean.png');
+//});
+//
+//app.get('/script.js', function(req, res) {
+//    res.sendfile(__dirname + '/script.js');
+//});
+//
+//app.get('/webrtc.io.js', function(req, res) {
+//    res.sendfile(__dirname + '/webrtc.io.js');
+//});
+
+x
