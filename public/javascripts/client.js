82a17a3eb881259b1729bba6b3797cecaddd811e
Joel Richard <joelrich@adobe.com>
2014-04-24 21:20:28 +0200

    First version of chat

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,8 +1,5 @@
 (function () {
     var ownPeerId = window.prompt("Username");
-
-    var users = {};
-
     var peer = window.peer = new Peer(ownPeerId, {host: 'localhost', port: 9000, path: '/chat'});
     peer.listAllPeers(function (users) {
         users.forEach(function (peerId) {
@@ -14,11 +11,34 @@
     });
 
     function addUser(conn) {
-        var $el = $("<li>").text(conn.peer).appendTo("ul.users");
-        conn.on('close', function () { // TODO not called?
+        var $el = $("<li>").text(conn.peer).appendTo(".users");
+        conn.on('data', function (data) {
+            addMessage(conn.peer, data);
+        });
+        conn.on('close', function () {
             $el.remove();
         });
     }
+
+    function addMessage(peerId, text) {
+        $("p").text(text).appendTo(".log");
+
+    }
+
+    $(function () {
+        $("form").on("submit", function (e) {
+            e.preventDefault();
+
+            var $input = $("input");
+            var message = $input.val();
+            addMessage(peer.id, message);
+            $input.val("");
+
+            for (var id in peer.connections) {
+                peer.connections[id][0].send(message);
+            }
+        });
+    });
 
 })();
 
